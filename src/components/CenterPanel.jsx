42944e223671007a4bde4981ebdf0f21f0056823
joki <josedavidamayar@gmail.com>
2024-04-30 03:17:34 -0500

    Estilos al panel central

--- src/components/CenterPanel.jsx
+++ src/components/CenterPanel.jsx
@@ -1,5 +1,15 @@
+import './CenterPanel.css'
+
 export function CenterPanel () {
     return (
-        <h3>Prueba del contenido del centro</h3>
+        <div className='draw-template'>
+            <div className='graph-area'>
+
+            </div>
+            <div className='button-area'>
+                <button>Ingresar Nodo</button>
+                <button>Ingresar Arista</button>
+            </div>
+        </div>
     )
 }
