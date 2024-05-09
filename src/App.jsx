2bbbea613af8002e069d17008ea0864bb5415dd3
joki <josedavidamayar@gmail.com>
2024-05-08 23:05:38 -0500

    Manejo de links para navegar

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [grafo, setGrafo] = useState(new Grafo())
   const [leftPanelKey, setLeftPanelKey] = useState(0)
   const [rightPanelKey, setRightPanelKey] = useState(0)
+  const [mainContent, setMainContent] = useState('grafo')
 
   const actualizarGrafo = nuevoGrafo => {
     setGrafo(nuevoGrafo);
@@ -18,22 +19,14 @@ function App() {
     setRightPanelKey(prevKey => prevKey + 1)
   }
 
-  return (
-    <div className='content'>
+  //Actualiza el contenido del main dependiendo a el link al que se le da click
+  const handleContentClick = (content) => {
+    setMainContent(content)
+  }
 
-      <header>
-        <div className='logo-name'>
-          <img className='img-logo' src={pigImage} alt="pig logo" />
-          <h1>PIG</h1>
-        </div>
-        <div className='links-top'>
-          <ul className='links'>
-            <li><a>Construcción de grafos</a></li>
-            <li><a>Otros...</a></li>
-          </ul>
-        </div>
-      </header>
-
+  //Renderiza el apartado del contenido del grafo
+  const renderContentGrafo = () => {
+    return (
       <main>
         <div className='left-panel'>
           <LeftPanel key={leftPanelKey} grafo={grafo} />
@@ -47,6 +40,46 @@ function App() {
           <RightPanel key={rightPanelKey} grafo={grafo} />
         </div>
       </main>
+    )
+  }
+
+  //Rnderiza el apartado del contenido de arboles
+  const renderContentArbol = () => {
+    return (
+      <main>
+        <h3>Prueba de renderizado de contenido del árbol</h3>
+      </main>
+    )
+  }
+
+  const renderGlosario = () => {
+    return (
+      <main>
+        <h3>Prueba de renderizado del glosario</h3>
+      </main>
+    )
+  }
+
+  return (
+    <div className='content'>
+
+      <header>
+        <div className='logo-name'>
+          <img className='img-logo' src={pigImage} alt="pig logo" />
+          <h1>PIG</h1>
+        </div>
+        <div className='links-top'>
+          <ul className='links'>
+            <li><a onClick={() => handleContentClick('grafo')}>Construcción de grafos</a></li>
+            <li><a onClick={() => handleContentClick('arbol')}>Construcción de árboles</a></li>
+            <li><a onClick={() => handleContentClick('glosario')}>Glosario</a></li>
+          </ul>
+        </div>
+      </header>
+
+      {mainContent === 'grafo' && renderContentGrafo()}
+      {mainContent === 'arbol' && renderContentArbol()}
+      {mainContent === 'glosario' && renderGlosario()}
 
     </div>
   )
